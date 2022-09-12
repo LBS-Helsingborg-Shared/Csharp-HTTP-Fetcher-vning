218833f0cd93f26a6dea8eaac37cad2c3e5ee78e
Mathias Olausson <88106166+realmatotes@users.noreply.github.com>
2022-09-12 17:09:02 +0200

    added shell

--- HTTP Fetcher/Program.cs
+++ HTTP Fetcher/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Text.Json;
+
+
+HttpWebRequest httpRequest = HttpWebRequest.CreateHttp("https://api.scb.se/OV0104/v1/doris/en/ssd/HE/HE0110/HE0110A/SamForvInk2");
+httpRequest.Method = "GET";
+WebResponse response = httpRequest.GetResponse();
+
+JsonDocument statistik = JsonDocument.Parse(response.GetResponseStream());
+
+foreach(var statistic in statistik.RootElement.EnumerateObject())
+{
+    if(statistic.Name == "title")
+    {
+        Console.WriteLine("Titel");
+        Console.WriteLine(statistic.Value);
+    }
+}
+
+Console.WriteLine(statistik.ToString());
